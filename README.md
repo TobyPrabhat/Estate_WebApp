84dc80b04af4bdb8cc46f2e17018e1357df6ba2d
Prabhat <devprodi.start@gmail.com>
2023-10-07 18:27:22 +0530

    Added Pages and their routes

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Estate App
 
 1. To run this app first clone the repository 
 2. Run npm command to install all the neccessary files.
+3. Run npm run dev to run the web application.
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import SignUp from './Pages/SignUp'
+import About from './Pages/About'
+import Profile from './Pages/Profile'
+import SignIn from './Pages/SignIn'
+import Home from './Pages/Home'
+
+
 
 export default function App() {
-  return (
-    <div className='text-red-500'>App</div>
-  )
+  return <BrowserRouter>
+  <Routes>
+    <Route path='/' element = {<Home />} />
+    <Route path='/about' element = {<About />} />
+    <Route path='/profile' element = {<Profile />} />
+    <Route path='/sign-in' element = {<SignIn />} />
+    <Route path='/sign-out' element = {<SignUp />} />
+  </Routes>
+  </BrowserRouter>
 }
