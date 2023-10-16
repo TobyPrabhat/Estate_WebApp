ac5ac4c9862a2442dc903b4cc63f59375ebee787
Prabhat <devprodi.start@gmail.com>
2023-10-16 11:52:59 +0530

    Created a middleware and function for possible errors

--- api/controllers/userAuthController.js
+++ api/controllers/userAuthController.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const {name, email, password} = req.body;
     const hashPassword = bcryptjs.hashSync(password, 10);
     const NewUser = new User({name, email, password: hashPassword});
@@ -9,6 +9,6 @@ export const signup = async (req, res) => {
         await NewUser.save();
         res.status(201).json('user successfully saved');
     } catch (error) {
-        res.status(500).json(error.message);
+        next(error);
     }
 };
