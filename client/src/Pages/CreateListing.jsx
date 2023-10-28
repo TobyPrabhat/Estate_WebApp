5c118f909ba43e7c8d3de5631f22603082d97df1
Prabhat <devprodi.start@gmail.com>
2023-10-28 11:18:26 +0530

    create get listing api route

--- client/src/Pages/CreateListing.jsx
+++ client/src/Pages/CreateListing.jsx
@@ -127,7 +127,7 @@ export default function CreateListing() {
       if(data.success === false){
         setError(data.message);
       }
-      navigate(`/listing/${data._id}`);
+      navigate(`/listings/${data._id}`);
     } catch (error) {
       setError(error.message);
       setLoading(false);
